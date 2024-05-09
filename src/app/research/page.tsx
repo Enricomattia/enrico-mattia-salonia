5916f9995b6bfc55ed5c543c3d7ff0f99c8b9293
Enrico <mattia.salonia1@gmail.com>
2024-05-08 19:48:38 -0500

    work in progress other work

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -9,7 +9,7 @@ export default function Research() {
   return (
     <main className="main col">
       <PageSection>
-        <SectionTitle text="Work in progress"/>
+        <SectionTitle text="Work in Progress"/>
         <CardExtended title="A Foundation for Universalisation in Games">
           <Abstract 
             content="In revealed preference theory, observed choice is interpreted as revealing tastes over the outcomes of the decision. Nevertheless, if a moral principle prescribes an act for reasons unrelated to its consequences, the inference drawn regarding preferences is misleading. In this paper, I study the behaviour of deontological decision makers who follow the moral principle of universalisation. I develop a decision theory for agents who value the impact of their choice in determining a counterfactual outcome they envision. Hence, the choice of action reveals a preference for counterfactual outcomes. I propose a unifying model based on my theory, inspired by the equal sacrifice principle. It can be specified to obtain the most prominent models of universalisation, compare them, highlight and arguably overcome their limitations."
@@ -25,7 +25,7 @@ export default function Research() {
         </CardExtended>
       </PageSection>
       <PageSection>
-        <SectionTitle text="Other Works"/>
+        <SectionTitle text="Other Work"/>
         <TextSimple content="I have some work in areas in the neighborhood of economics, but not quite there. You can find it here."/>
         <CardExtended title="The Chaining Argument Unchained" authors="Annalisa Costella" links="https://www.annalisacostella.com/home">
           <Abstract content="We show that formalising Chang's chaining argument against the Trichotomy Thesis demonstrates that the intuitive pull of the argument relies on exploiting the features of a notion of closeness that is, however, a trichotomous relation itself. As such, the premises of the argument are inconsistent with each other, and the argument is unsound. Our results illuminate that the chaining argument does not provide insights into axiology."/>
