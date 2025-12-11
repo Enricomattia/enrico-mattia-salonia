df709b0d069c46ae06bc73e1d0eeb90b4c677780
Enrico <mattia.salonia1@gmail.com>
2025-12-11 15:07:08 +0100

    adjusted spacing teaching

--- src/app/teaching/page.tsx
+++ src/app/teaching/page.tsx
@@ -7,12 +7,23 @@ export default function Teaching() {
     <main className="main col">
       <PageSection>
         <SectionTitle text="Classes" />
-        <ul className="list-disc pl-8">
+        <ul className="list-disc pl-8 flex flex-col gap-y-3">
           <li>
-            <Link href="/teaching/microeconomics" text="Microeconomics I (Fall 2025), First year PhD Tor Vergata" size="text-l" target="_self" />
+            <Link
+              href="/teaching/microeconomics"
+              text="Microeconomics I (Fall 2025), First year PhD Tor Vergata"
+              size="text-l"
+              target="_self"
+            />
           </li>
+          <br />
           <li>
-            <Link href="/teaching/topics-in-macro" text="Topics in Macro I (Fall 2020-2023), Third year bachelor Toulouse" size="text-l" target="_self" />
+            <Link
+              href="/teaching/topics-in-macro"
+              text="Topics in Macro I (Fall 2020-2023), Third year bachelor Toulouse"
+              size="text-l"
+              target="_self"
+            />
           </li>
         </ul>
       </PageSection>
