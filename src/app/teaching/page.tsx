067e2b7446ed5896d560b2c142a9884b481f5765
Enrico <mattia.salonia1@gmail.com>
2025-12-22 18:43:19 +0100

    restructured teaching

--- src/app/teaching/page.tsx
+++ src/app/teaching/page.tsx
@@ -2,29 +2,63 @@ import Link from "@/components/atoms/Link/Link";
 import PageSection from "@/components/atoms/PageSection/PageSection";
 import SectionTitle from "@/components/atoms/SectionTitle/SectionTitle";
 
+const classes = [
+  {
+    title: "Microeconomics I",
+    meta: "(Fall 2025), First year PhD Tor Vergata",
+    notesHref: "/notes/main.pdf",
+    syllabusHref: "/teaching/microeconomics/syllabus",
+  },
+  {
+    title: "Topics in Macro I",
+    meta: "(Fall 2020-2023), Third year bachelor Toulouse",
+    notesHref: "/docs/notes_macro.pdf",
+    syllabusHref: "/docs/macro_syl.pdf",
+    solowSwanHref: "https://mybinder.org/v2/gh/Enricomattia/TopicsInMacro1TD/HEAD?filepath=solow-swan.ipynb",
+  },
+];
+
 export default function Teaching() {
   return (
     <main className="main col">
       <PageSection>
         <SectionTitle text="Classes" />
-        <ul className="list-disc pl-8 flex flex-col gap-y-3">
-          <li>
-            <Link
-              href="/teaching/microeconomics"
-              text="Microeconomics I (Fall 2025), First year PhD Tor Vergata"
-              size="text-l"
-              target="_self"
-            />
-          </li>
-          <br />
-          <li>
-            <Link
-              href="/teaching/topics-in-macro"
-              text="Topics in Macro I (Fall 2020-2023), Third year bachelor Toulouse"
-              size="text-l"
-              target="_self"
-            />
-          </li>
+
+        <ul className="list-disc pl-8 flex flex-col gap-y-10">
+          {classes.map((c) => {
+            const resources = [
+              { href: c.notesHref, text: "Notes" },
+              { href: c.syllabusHref, text: "Syllabus" },
+              ...(c.solowSwanHref ? [{ href: c.solowSwanHref, text: "Solow-Swan" }] : []),
+            ];
+
+            return (
+              <li key={c.title} className="flex flex-col gap-y-4">
+                <div className="text-l">
+                  <strong>{c.title}</strong> <span>{c.meta}</span>
+                </div>
+
+                <br />
+
+                <div className="flex flex-wrap items-center pl-1">
+                  {resources.map((r, idx) => (
+                    <span key={r.href} className="flex items-center">
+                      {idx > 0 && (
+                        <>
+                          <span style={{ display: "inline-block", width: 14 }} />
+                          <span className="opacity-60">•</span>
+                          <span style={{ display: "inline-block", width: 14 }} />
+                        </>
+                      )}
+                      <Link href={r.href} text={r.text} size="text-m" target="_blank" />
+                    </span>
+                  ))}
+                </div>
+
+                <br />
+              </li>
+            );
+          })}
         </ul>
       </PageSection>
     </main>
