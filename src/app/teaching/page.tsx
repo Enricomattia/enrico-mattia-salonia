31e3216d556901f72cad275bf80528f2094aeed3
Enrico <mattia.salonia1@gmail.com>
2026-02-18 11:51:41 +0100

    syllabus game theory and cv

--- src/app/teaching/page.tsx
+++ src/app/teaching/page.tsx
@@ -4,15 +4,20 @@ import SectionTitle from "@/components/atoms/SectionTitle/SectionTitle";
 
 const classes = [
   {
+    title: "Game Theory",
+    meta: "(Spring 2026), Second year bachelor, Tor Vergata",
+    syllabusHref: "/docs/game_syl.pdf",
+  },
+  {
     title: "Microeconomics I",
-    meta: "(Fall 2025), First year PhD Tor Vergata",
+    meta: "(Fall 2025), First year PhD, Tor Vergata",
     notesHref: "/docs/notes_micro.pdf",
     solutionsHref: "/docs/solutions_micro.pdf",
     syllabusHref: "/docs/micro_syl.pdf",
   },
   {
     title: "Topics in Macro I",
-    meta: "(Fall 2020-2023), Third year bachelor Toulouse",
+    meta: "(Fall 2020-2023), Third year bachelor, Toulouse",
     notesHref: "/docs/notes_macro.pdf",
     syllabusHref: "/docs/macro_syl.pdf",
     solowSwanHref: "https://mybinder.org/v2/gh/Enricomattia/TopicsInMacro1TD/HEAD?filepath=solow-swan.ipynb",
@@ -28,9 +33,9 @@ export default function Teaching() {
         <ul className="list-disc pl-8 flex flex-col gap-y-10">
           {classes.map((c) => {
             const resources = [
-              { href: c.notesHref, text: "Notes" },
+              ...(c.notesHref ? [{ href: c.notesHref, text: "Notes" }] : []),
               ...(c.solutionsHref ? [{ href: c.solutionsHref, text: "Solutions" }] : []),
-              { href: c.syllabusHref, text: "Syllabus" },
+              ...(c.syllabusHref ? [{ href: c.syllabusHref, text: "Syllabus" }] : []),
               ...(c.solowSwanHref ? [{ href: c.solowSwanHref, text: "Solow-Swan" }] : []),
             ];
 
